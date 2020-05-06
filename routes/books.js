16935094a93d6ac3dd7ab8f63877110ae33b7df2
Monika Frankowska <monia.frankowska@gmail.com>
2020-05-06 16:18:16 +0200

    routes error handlers

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
 
+//variable checking which error to throw
+let checkError;
+
 /* Handler function to wrap each route. */
 function asyncHandler(callbackF){
     return async(req, res, next) => {
@@ -18,10 +21,12 @@ router.get('/', asyncHandler(async (req, res) => {
     const books = await Book.findAll({ order: [['createdAt', 'DESC']]});
     res.render('index', {books, title: 'All Books'});
 } ));
+
 /* GET new book form */
 router.get('/new', asyncHandler(async (req, res) => {
     res.render('new-book', {book: {}, title: 'New Book'});
 } ));
+
 /* POST new book form */
 router.post('/new', asyncHandler(async (req, res) => {
     let book;
@@ -31,15 +36,51 @@ router.post('/new', asyncHandler(async (req, res) => {
 } ));
 
 /* GET update book form */
-router.get('/:id', asyncHandler(async (req, res) => {
+router.get('/:id', asyncHandler(async (req, res, next) => {
     const book = await Book.findByPk(req.params.id); //finds book by its id
     // console.log(book);
+    let err;
     book
         ?res.render('update-book', { book, title: book.title, h1: 'Update' })
-        :res.sendStatus(404);
+        :(err = new Error('Book Page Not Found'), //create 404 status error
+        err.statusCode = 404,
+        checkError = false,
+        next(err))
 } ));
 
 /* POST update books info in a database */
+router.post('/:id', asyncHandler(async (req, res) => {
+    let book;
+    book = await Book.findByPk(req.params.id); //finds book by its id
+    book
+        ?(await book.update(req.body),
+        res.redirect('/books/' + book.id))
+        :res.sendStatus(404)
+} ));
 
+/* POST delete book */
+router.post('/:id/delete', asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    book
+        ?(await book.destroy(),
+        res.redirect('/books'))
+        :res.sendStatus(404)
+} ));
+
+//error handling
+router.get('*', function(req, res, next) { //for any not existing rout
+    let err = new Error('Page Not Found'); //create 404 status error
+    err.statusCode = 404;
+    next(err);
+});
+
+/* Error handler */
+router.use((err, req, res, next)=> {
+    res.locals.error = err;
+    console.log('Error: Something went wrong');
+    (checkError === false)
+        ?res.render('page-not-found') //render error page
+        :res.render('error')
+} );
 
 module.exports = router;
